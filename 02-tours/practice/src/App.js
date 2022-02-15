68fa7e2a9c3a360561cd975425459422f196537f
Kaushik Chemburkar <attherate@gmail.com>
2022-02-16 08:50:54 +1100

    we will first see the loading component. Then we useState an empty array for tours and before we fetch the tours data.
    Then we async/await fetch tours info from the api

--- 02-tours/practice/src/App.js
+++ 02-tours/practice/src/App.js
@@ -6,8 +6,26 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      console.log(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+
+    // console.log(tours)
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
   if (loading) {
     return (
       <main>
