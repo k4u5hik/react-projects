63192beac740d6411bb245bc94782504e31f14bc
Kaushik Chemburkar <attherate@gmail.com>
2022-02-16 10:20:00 +1100

    - added message if there are no tours - added a refresh button to show all tours

--- 02-tours/practice/src/Loading.js
+++ 02-tours/practice/src/Loading.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="loading">
+    <div className='loading'>
       <h1>loading...</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
