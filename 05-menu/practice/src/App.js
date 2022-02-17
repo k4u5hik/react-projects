1c02a4629382fc27d2d7c589fc5aa31adbf92eb0
Kaushik Chemburkar <attherate@gmail.com>
2022-02-17 16:05:18 +1100

    problem - we are not in sync with the data. If there is new data and categories, we would have to do this process manually. Hence we need to automate the process. - some category names are repeating in the list, we only need to add it once. We can get that result with new Set() to get the unique values

--- 05-menu/practice/src/App.js
+++ 05-menu/practice/src/App.js
@@ -3,9 +3,13 @@ import Menu from './Menu'
 import Categories from './Categories'
 import items from './data'
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+// check if the values are unique
+console.log(allCategories)
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
 
   const filterItems = (category) => {
     if (category === 'all') {
