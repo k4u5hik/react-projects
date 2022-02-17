de7b1c4a1ad7961791f728122802aedeea2d6a7d
Kaushik Chemburkar <attherate@gmail.com>
2022-02-17 14:30:26 +1100

    setting up menu and categories

--- 05-menu/practice/src/App.js
+++ 05-menu/practice/src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItem, setMenuItem] = useState(items)
+  const [categories, setCategories] = useState([])
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
