db8fface0c82de7cafa08b2bc4fc517724d68634
Kaushik Chemburkar <attherate@gmail.com>
2022-02-18 23:03:59 +1100

    negative index bug fixed

--- 07-slider/practice/src/App.js
+++ 07-slider/practice/src/App.js
@@ -5,6 +5,13 @@ import data from './data'
 function App() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+  }, [index, people])
   return (
     <section className='section'>
       <div className='title'>
